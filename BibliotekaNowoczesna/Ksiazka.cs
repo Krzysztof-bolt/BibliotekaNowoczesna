1aa32f847852fe498f7da816766710eef57dff50
Krzysztof Wołowski <78325693+Krzysztof-bolt@users.noreply.github.com>
2024-05-21 14:58:26 +0200

    Klasa ksiiazka z konstruktorami

--- BibliotekaNowoczesna/Ksiazka.cs
+++ BibliotekaNowoczesna/Ksiazka.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics.CodeAnalysis;
 
 namespace BibliotekaNowoczesna;
 
     public class Ksiazka
     {
     public Ksiazka() { }
+
+    [SetsRequiredMembers]
     public Ksiazka(string? isbn, string? tytul)
     {
         Isbn = isbn;
